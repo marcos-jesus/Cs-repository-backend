121c0b0406a4e4dac0e581373381f1369c110a5e
Marcao <marcos.jesus@cliqx.com.br>
2023-09-11 10:26:34 -0300

    add: Methods with Parameters

---
+++ methodParameter/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("#Método com Parametro");
+
+
+Calculadora calc = new Calculadora();
+
+var valor = calc.Subtrair(10,10);
+
+Console.WriteLine(valor);
+
+Console.ReadKey();
+class Calculadora
+{
+    public int Somar(int n1, int n2)
+    {
+        return n1 + n2;
+    } public int Subtrair(int n1, int n2)
+    {
+        return n1 - n2;
+    } public int Multiplicar(int n1, int n2)
+    {
+        return n1 * n2;
+    } public int Dividir(int n1, int n2)
+    {
+        return n1 / n2;
+    }
+
+}
