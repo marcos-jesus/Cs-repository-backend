c519bd977930eaf67fc47fb92e53d39bbbecb2d1
Marcao <marcos.jesus@cliqx.com.br>
2023-09-12 10:00:06 -0300

    add: Passagem de argumento por valor e ref

---
+++ ArgumentoPorValor/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Passagem de argumentos por valor");
+
+int x = 20;
+
+Console.WriteLine("\n Valor do argumento x antes de passar por valor " + x);
+
+Calculo calc = new Calculo();
+calc.Dobrar(x);
+
+Console.WriteLine("\n Valor do argumento x depois de passar por valor " + x);
+
+Console.ReadKey();
+
+
+public class Calculo
+{
+    public void Dobrar(int y)
+    {
+        y *= 2;
+        Console.WriteLine("\n Valor do parâmetro y no método dobrar " + y);
+    }
+}
