bca909a8fbe50b0c19ae3e2a57d80c07ae06bf49
Marcao <marcos.jesus@cliqx.com.br>
2023-09-16 21:22:34 -0300

    add: Enum

---
+++ Enum/Enumerations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    enum Categorias
+    {
+        Moda = 1,
+        Automotivo,
+        Artes,
+        Eletronicos,
+        Livros,
+        Brinquedos
+    }
+
+    enum DiasDasemana
+    {
+        Segunda,
+        Terça,
+        Quarta,
+        Quinta,
+        Sexta,
+        Sabado = 7,
+        Domingo,
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿using Enum;
+
+Console.WriteLine("Enum");
+
+//Console.WriteLine(DiasDasemana.Sabado);
+//Console.WriteLine(DiasDasemana.Segunda);
+
+//int dia1 = (int)DiasDasemana.Sabado;
+//int dia2 = (int)DiasDasemana.Segunda;
+//int dia3 = (int)DiasDasemana.Domingo;
+
+
+
+//Console.WriteLine(dia1);
+//Console.WriteLine(dia2);
+//Console.WriteLine(dia3);
+
+
+Console.WriteLine("Selecione uma categoria");
+Console.WriteLine($"{Categorias.Moda}");
+Console.WriteLine($"{Categorias.Automotivo}");
+Console.WriteLine($"{Categorias.Artes}");
+Console.WriteLine($"{Categorias.Eletronicos}");
+Console.WriteLine($"{Categorias.Livros}");
+Console.WriteLine($"{Categorias.Brinquedos}");
+
+int valor = Convert.ToInt32(Console.ReadLine());
+var nomeEnum = (Categorias)valor;
+
+Console.WriteLine($"Você selecionou a categoria: {nomeEnum.ToString()}");
+Console.ReadKey();
